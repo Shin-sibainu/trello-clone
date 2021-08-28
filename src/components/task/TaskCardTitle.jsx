05cf8595f04f2b656796eb15bd76a8184d1e38bc
Shin-sibainu <shincode0712@gmail.com>
2021-08-28 11:01:30 +0900

    taskcardTitleinput complete

--- src/components/task/TaskCardTitle.jsx
+++ src/components/task/TaskCardTitle.jsx
@@ -1,5 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const TaskCardTitle = () => {
-  return <div></div>;
+  const [isClick, setIsClick] = useState(false);
+  const [inputCardTitle, setInputCardTitle] = useState("Today");
+
+  const handleClick = () => {
+    setIsClick(true);
+    console.log(isClick);
+  };
+
+  const handleChange = (e) => {
+    console.log(inputCardTitle);
+    setInputCardTitle(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsClick(false);
+  };
+
+  const handleBlur = () => {
+    setIsClick(false);
+  };
+
+  return (
+    <div onClick={handleClick} className="taskCardTitleInputArea">
+      {isClick ? (
+        <form onSubmit={handleSubmit}>
+          <input
+            className="taskCardTitleInput"
+            autoFocus
+            type="text"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={inputCardTitle}
+            maxLength="10"
+          />
+        </form>
+      ) : (
+        <h3>{inputCardTitle}</h3>
+      )}
+    </div>
+  );
 };
